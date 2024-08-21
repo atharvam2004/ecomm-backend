803d00dc0f71e7b6436a89d68624114bd9bf7a12
atharvam2004 <meheratharva49@gmail.com>
2024-08-22 02:17:00 +0530

    made login persistent

--- model/User.js
+++ model/User.js
@@ -6,11 +6,10 @@ const userSchema = new Schema({
   password: { type: Buffer, required: true },
   role: { type: String, required: true, default:'user' },
   addresses: { type: [Schema.Types.Mixed] }, 
-  // for addresses, we can make a separate Schema like orders. but in this case we are fine
+  // TODO:  We can make a separate Schema for this
   name: { type: String },
-  salt: Buffer,
-  resetPasswordToken: {type: String, default:''}
-},{timestamps: true});
+  salt: Buffer
+});
 
 const virtual = userSchema.virtual('id');
 virtual.get(function () {
